d682829ddb80956418ca9d61cf9796b382353eb7
Ruslan Garaev <r.garaevs@gmail.com>
2022-12-27 22:16:19 +0300

    Старт проекта. Верстка заголовка и второго блока рекламы

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import logoSvg from './assets/images/logo.svg';
+import cartSvg from './assets/images/cart.svg';
+import favoriteSvg from './assets/images/favorite.svg';
+import profileSvg from './assets/images/profile.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='wrapper'>
+			<header className='header'>
+				<div className='logo'>
+					<img src={logoSvg} alt='logo' />
+					<div className='logoName'>
+						<div className='logoTitle'>React Snikers</div>
+						<div className='logoSubtitle'>Магазин лучших кроссовок</div>
+					</div>
+				</div>
+				<div className='profile'>
+					<div className='myCart'>
+						<img src={cartSvg} alt='cart' />
+						<p className='price'>1205 руб.</p>
+					</div>
+					<img src={favoriteSvg} className='favorite' alt='favorite' />
+					<img src={profileSvg} className='profileSvg' alt='cabinet' />
+				</div>
+			</header>
+			<div className='reklama'>
+				<img src='/assets/images/reklama.png' alt='reklama' />
+			</div>
+		</div>
+	);
 }
 
 export default App;
