b4f6cd39ba6b0084874c4ec47ad2efa3543d1cbd
Ruslan Garaev <r.garaevs@gmail.com>
2023-02-07 20:49:01 +0300

    Заглушка страницы закладок

--- src/components/Shop/index.tsx
+++ src/components/Shop/index.tsx
@@ -5,6 +5,7 @@ import styles from './Shop.module.scss';
 import Title from '../Title';
 // import CartItem from '../CartItem';
 import Item from '../Item';
+import { Link } from 'react-router-dom';
 
 type Items = {
 	title: string;
@@ -26,7 +27,18 @@ const Shop: React.FC<ItemArray> = ({ items, title }) => {
 		<div className={styles.shop}>
 			<Title title={title} />
 			<div className={styles.shop__catalog}>
-				{items.length > 0 ? items.map(item => <Item key={item.id} {...item} />) : 'No shop'}
+				{items.length > 0 ? (
+					items.map(item => <Item key={item.id} {...item} />)
+				) : (
+					<div className={styles.no__items}>
+						<img className={styles.smile} src='/assets/images/no-cart.png' alt='Нет товаров' />
+						<p className={styles.no__title}>Закладок нет :(</p>
+						<p className={styles.no__subtitle}>Вы ничего не добавляли в закладки</p>
+						<Link to={'/'}>
+							<button>Вернуться на главную</button>
+						</Link>
+					</div>
+				)}
 			</div>
 		</div>
 	);
