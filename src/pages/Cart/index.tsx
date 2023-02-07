770a049f1983396c1c8252e190efbb5e45ed4be8
Ruslan Garaev <r.garaevs@gmail.com>
2023-02-07 20:30:10 +0300

    Редактировал страницу корзины: заглушка

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -2,11 +2,13 @@ import Header from '../../components/Header';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 
 import styles from './Cart.module.scss';
+import buttonSvg from '../../assets/images/buttonCart.svg';
 
 // import Shop from '../../components/Shop';
 import Title from '../../components/Title';
 import CartItem from '../../components/CartItem';
 import { clearCartSlice } from '../../store/items/cartSlice';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
 	const dispatch = useAppDispatch();
@@ -21,18 +23,36 @@ const Cart = () => {
 	return (
 		<>
 			<Header />
-			{/* <Shop items={items} title={'Корзина'} /> */}
 			<div className={styles.shop}>
 				<Title title={'Корзина'} />
 				<div className={styles.shop__catalog}>
-					{items.length > 0 ? items.map(item => <CartItem key={item.id} {...item} />) : 'No shop'}
+					{items.length > 0 ? (
+						items.map(item => <CartItem key={item.id} {...item} />)
+					) : (
+						<div className={styles.no__items}>
+							<img className={styles.smile} src='/assets/images/no-cart.png' alt='Нет товаров' />
+							<p className={styles.no__title}>У вас нет заказов</p>
+							<p className={styles.no__subtitle}>
+								Вы нищеброд?
+								<br />
+								Оформите хотя бы один заказ.
+							</p>
+							<Link to={'/'}>
+								<button>Вернуться на главную</button>
+							</Link>
+						</div>
+					)}
 				</div>
-				<div className={styles.cart__add}>
-					<p className={styles.price__info}>
-						Итоговая стоимость: <b>{sumItems > 0 ? sumItems : '0'} руб.</b>
-					</p>
-					<button onClick={() => onClearCart()}>Очистить корзину</button>
-				</div>
+				{items.length > 0 ? (
+					<div className={styles.cart__add}>
+						<p className={styles.price__info}>
+							Итоговая стоимость: <b>{sumItems > 0 ? sumItems : '0'} руб.</b>
+						</p>
+						<button onClick={() => onClearCart()}>Очистить корзину</button>
+					</div>
+				) : (
+					''
+				)}
 			</div>
 		</>
 	);
